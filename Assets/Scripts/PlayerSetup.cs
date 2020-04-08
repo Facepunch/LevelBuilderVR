2d95ac5bc50f4f34a5f2cf12f2696e8922d25d18
James King <james.king@facepunchstudios.com>
2020-04-08 01:45:49 +0100

    Added SteamVR, got basic movement working

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+namespace LevelBuilderVR
+{
+    public class PlayerSetup : MonoBehaviour
+    {
+        public bool ShowController = true;
+
+        [UsedImplicitly]
+        private void Update()
+        {
+            var finishedSetup = false;
+
+            foreach (var hand in Player.instance.hands)
+            {
+                if (hand == null) continue;
+                if (hand.mainRenderModel == null) continue;
+
+                hand.SetSkeletonRangeOfMotion(ShowController ? EVRSkeletalMotionRange.WithController : EVRSkeletalMotionRange.WithoutController);
+                hand.ShowController(ShowController);
+
+                finishedSetup = true;
+            }
+
+            if (finishedSetup)
+            {
+                enabled = false;
+            }
+        }
+    }
+}
