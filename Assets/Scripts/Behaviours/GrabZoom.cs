6f3be51ce336fab06a2e43017743e87fd882388d
James King <james.king@facepunchstudios.com>
2020-04-10 23:06:31 +0100

    Started on GrabZoom, currently axis aligned

---
+++ Assets/Scripts/Behaviours/GrabZoom.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+namespace LevelBuilderVR.Behaviours
+{
+    public class GrabZoom : MonoBehaviour
+    {
+        public HybridLevel TargetLevel;
+
+        public SteamVR_Action_Boolean GrabZoomAction = SteamVR_Input.GetBooleanAction("GrabZoom");
+
+        private Vector3 _leftLocalAnchor;
+        private Vector3 _rightLocalAnchor;
+
+        private void OnEnable()
+        {
+
+        }
+
+        private static bool IsHandValid(Hand hand)
+        {
+            return hand.currentAttachedObject == null;
+        }
+
+        private void Update()
+        {
+            if (TargetLevel == null) return;
+
+            var player = Player.instance;
+
+            var leftValid = IsHandValid(player.leftHand);
+            var rightValid = IsHandValid(player.rightHand);
+
+            var leftGrabZoomPressed = leftValid && GrabZoomAction.GetStateDown(SteamVR_Input_Sources.LeftHand);
+            var rightGrabZoomPressed = rightValid && GrabZoomAction.GetStateDown(SteamVR_Input_Sources.RightHand);
+
+            var leftGrabZoomReleased = leftValid && GrabZoomAction.GetStateUp(SteamVR_Input_Sources.LeftHand);
+            var rightGrabZoomReleased = rightValid && GrabZoomAction.GetStateUp(SteamVR_Input_Sources.RightHand);
+
+            var leftGrabZoomHeld = leftValid && GrabZoomAction.GetState(SteamVR_Input_Sources.LeftHand);
+            var rightGrabZoomHeld = rightValid && GrabZoomAction.GetState(SteamVR_Input_Sources.RightHand);
+
+            var leftLocalPos = TargetLevel.transform.InverseTransformPoint(player.leftHand.objectAttachmentPoint.position);
+            var rightLocalPos = TargetLevel.transform.InverseTransformPoint(player.rightHand.objectAttachmentPoint.position);
+
+            if (leftGrabZoomPressed || rightGrabZoomPressed || leftGrabZoomReleased || rightGrabZoomReleased)
+            {
+                _leftLocalAnchor = leftLocalPos;
+                _rightLocalAnchor = rightLocalPos;
+            }
+
+            if (leftGrabZoomHeld && rightGrabZoomHeld)
+            {
+                var anchor = (_leftLocalAnchor + _rightLocalAnchor) * 0.5f;
+                var pos = (leftLocalPos + rightLocalPos) * 0.5f;
+                var localDiff = pos - anchor;
+
+                var worldDiff = TargetLevel.transform.TransformVector(localDiff);
+
+                TargetLevel.transform.Translate(worldDiff, Space.World);
+
+                var betweenAnchorDiff = _rightLocalAnchor - _leftLocalAnchor;
+                var betweenLocalDiff = rightLocalPos - leftLocalPos;
+
+                var sizeRatio = betweenLocalDiff.magnitude / betweenAnchorDiff.magnitude;
+
+                TargetLevel.transform.localScale *= sizeRatio;
+            }
+            else if (leftGrabZoomHeld || rightGrabZoomHeld)
+            {
+                var anchor = leftGrabZoomHeld ? _leftLocalAnchor : _rightLocalAnchor;
+                var pos = leftGrabZoomHeld ? leftLocalPos : rightLocalPos;
+                var localDiff = pos - anchor;
+                var worldDiff = TargetLevel.transform.TransformVector(localDiff);
+
+                TargetLevel.transform.Translate(worldDiff, Space.World);
+            }
+        }
+    }
+}
--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean GrabZoom
+        {
+            get
+            {
+                return SteamVR_Actions.default_GrabZoom;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
